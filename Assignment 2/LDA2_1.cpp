dfe95f8f39d850bf870e620658ae849c1e9fd331
AnonLuc <diaslucas12@hotmail.com>
2024-10-01 09:34:49 -0400

    Assignment 2 Commit

---
+++ Assignment 2/LDA2_1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Diasl on 10/1/2024.
+//
+
+#include "LDA2_1.h"
---
+++ Assignment 2/LDA2_1.h
@@ -0,0 +1,16 @@
+//
+// Created by Diasl on 10/1/2024.
+//
+
+#ifndef LDA2_1_H
+#define LDA2_1_H
+
+
+
+class LDA2_1 {
+
+};
+
+
+
+#endif //LDA2_1_H
